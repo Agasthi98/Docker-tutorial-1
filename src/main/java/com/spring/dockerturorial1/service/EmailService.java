fd3979d052406501654cf5ff204bd41de8f9745a
Agasthi98 <sankalana98@gmail.com>
2024-09-17 22:13:28 +0530

    remove unused imports

--- src/main/java/com/spring/dockerturorial1/service/EmailService.java
+++ src/main/java/com/spring/dockerturorial1/service/EmailService.java
@@ -2,8 +2,6 @@ package com.spring.dockerturorial1.service;
 
 import com.spring.dockerturorial1.model.EmailDetailsRequest;
 
-import java.util.Map;
-
 public interface EmailService {
     // Method
     // To send a simple email
